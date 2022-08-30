a202016ebd0aaa3f7f63e090dbdbbd2df4d4450a
Amit Kumar <amit.engg9897@gmail.com>
2022-08-30 23:05:03 +0530

    new

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ left.addEventListener('click', () => {
  
 right.addEventListener('click', () => {
     bx1.scrollLeft -= 100;
-});  
-
+});    
+ 
 const scroll_watch = document.getElementsByClassName("scroll_watch");
 const main_watch = document.getElementsByClassName("main_watch")[0];
 const img = document.getElementsByClassName("img")[0];
