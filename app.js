037edc455286c7af86063f348144d899864798fc
Amit Kumar <amit.engg9897@gmail.com>
2022-09-12 19:58:21 +0530

    new

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ scroll_watch[0].addEventListener('click', () =>{
         img.style.botton ="-230px"; 
     }, 1000);    
     setTimeout(() => {
-        img.src = "img/img2.png";      
+        img.src = "img/img2.png";        
     }, 1900);      
     setTimeout(() => {
         img.style.botton ="0px";
